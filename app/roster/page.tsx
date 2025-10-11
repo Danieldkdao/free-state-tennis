2fdafd7896a8d54ecf23c63d2bb18409e29ae8d8
Daniel Dao <danieldkdao@gmail.com>
2025-10-10 19:32:39 -0500

    setup and completed navbar

---
+++ app/roster/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RosterPage = () => {
+  return (
+    <div>RosterPage</div>
+  )
+}
+
+export default RosterPage
---
+++ components/Navbar.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "@/public/free-state-logo.png";
+import Link from "next/link";
+import { FaBars, FaArrowLeft } from "react-icons/fa6";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="w-full flex free-green-bg p-5 items-center justify-center">
+      <div className="w-full flex items-center justify-between max-w-[700px]">
+        <Link href="/" className="text-2xl font-bold sm:block hidden">
+          Home
+        </Link>
+        <Link href="/roster" className="text-2xl font-bold sm:block hidden">
+          Roster
+        </Link>
+        <Link href="https://www.freestateathletics.com/" target="_blank">
+          <Image src={Logo} alt="Free state logo" height={70} width={70} />
+        </Link>
+        <Link href="/schedule" className="text-2xl font-bold sm:block hidden">
+          Schedule
+        </Link>
+        <Link href="/news" className="text-2xl font-bold sm:block hidden">
+          News
+        </Link>
+        <button
+          onClick={() => setToggleMenu(true)}
+          className="cursor-pointer sm:hidden hover:opacity-50 active:scale-85 transition-all duration-200 ease-in-out"
+        >
+          <FaBars size={40} />
+        </button>
+      </div>
+      <div
+        className={`fixed inset-0 ${
+          toggleMenu ? "" : "translate-x-[100%]"
+        } transition-transform duration-400 ease-in-out bg-white flex flex-col`}
+      >
+        <button
+          onClick={() => setToggleMenu(false)}
+          className="cursor-pointer p-4 active:opacity-70 transition-opacity duration-300 ease-in-out border-b-2 border-gray-500/30"
+        >
+          <FaArrowLeft size={30} />
+        </button>
+        <Link
+          href="/"
+          className="text-2xl font-bold p-4 border-b-2 border-gray-500/30 hover:bg-[#054721] hover:text-white transition-colors duration-200 ease-in-out"
+        >
+          Home
+        </Link>
+        <Link
+          href="/roster"
+          className="text-2xl font-bold p-4 border-b-2 border-gray-500/30 hover:bg-[#054721] hover:text-white transition-colors duration-200 ease-in-out"
+        >
+          Roster
+        </Link>
+        <Link
+          href="/schedule"
+          className="text-2xl font-bold p-4 border-b-2 border-gray-500/30 hover:bg-[#054721] hover:text-white transition-colors duration-200 ease-in-out"
+        >
+          Schedule
+        </Link>
+        <Link
+          href="/news"
+          className="text-2xl font-bold p-4 border-b-2 border-gray-500/30 hover:bg-[#054721] hover:text-white transition-colors duration-200 ease-in-out"
+        >
+          News
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
